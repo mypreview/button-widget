c8b1b1edb7c134edc72f78e0a60ee49cb73f89bf
Mahdi Yazdani <mahdiyazdani@mail.com>
2019-12-19 13:41:31 +0300

    Empty file

---
+++ uninstall.php
@@ -0,0 +1,14 @@
+<?php
+/**
+ * Unistall Button Widget.
+ * Fired when the plugin is uninstalled.
+ *
+ * @author      Mahdi Yazdani
+ * @package     Button Widget
+ * @since       1.0.0
+ */
+
+// If uninstall not called from WordPress, then exit.
+if ( ! defined( 'WP_UNINSTALL_PLUGIN' ) ) {
+	exit;
+} // End If Statement
